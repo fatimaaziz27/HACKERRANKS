82a3a2d0cb5c474306d0b94c185fd67147f62a55
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-06 02:17:50 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] answer = new int[nums.length];
+        
+        return answer;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
